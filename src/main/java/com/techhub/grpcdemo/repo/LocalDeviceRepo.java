851b3abe4960b5c4dfb2e00569e3b9e6e2d7edcb
Ram Niwash <rniwash122@gmail.com>
2024-11-28 22:38:40 +0530

    Updated the device repo

--- src/main/java/com/techhub/grpcdemo/repo/LocalDeviceRepo.java
+++ src/main/java/com/techhub/grpcdemo/repo/LocalDeviceRepo.java
@@ -19,35 +19,44 @@ public class LocalDeviceRepo implements DeviceRepo {
         List<Device> devices = new ArrayList<>();
 
         Context context = new Context();
-        int result = LibUsb.init(context);
+        LibUsb.init(context);
 
         DeviceList devicesList = new DeviceList();
         LibUsb.getDeviceList(context, devicesList);
 
         for (org.usb4java.Device device : devicesList) {
-            /* Create Device Builder*/
-            Device.Builder builder = Device.newBuilder();
 
             DeviceDescriptor deviceDescriptor = new DeviceDescriptor();
             LibUsb.getDeviceDescriptor(device, deviceDescriptor);
 
             DeviceHandle deviceHandle = new DeviceHandle();
-            result = LibUsb.open(device, deviceHandle);
+            int result = LibUsb.open(device, deviceHandle);
+            StringBuffer buffer = null;
             if (result == 0) {
-                StringBuffer buffer = new StringBuffer(1024);
-                int byteRead = LibUsb.getStringDescriptorAscii(deviceHandle, deviceDescriptor.iProduct(), buffer);
-                String vendorId = Integer.toHexString((int) deviceDescriptor.idVendor());
-                String productId = Integer.toHexString((int) deviceDescriptor.idProduct());
-                vendorId = String.format("%4s", vendorId).replace(' ', '0');
-                productId = String.format("%4s", productId).replace(' ', '0');
-                builder.setDeviceDescription(buffer.toString())
-                        .setVendorId(vendorId)
-                        .setProductId(productId);
-                /* Add Device to list */
-                devices.add(builder.build());
+                buffer = new StringBuffer(1024);
+                LibUsb.getStringDescriptorAscii(deviceHandle, deviceDescriptor.iProduct(), buffer);
             }
-        }
+            String vendorId = Integer.toHexString(deviceDescriptor.idVendor());
+            String productId = Integer.toHexString(deviceDescriptor.idProduct());
+            vendorId = String.format("%4s", vendorId).replace(' ', '0');
+            productId = String.format("%4s", productId).replace(' ', '0');
 
+            System.out.println("|******* DEVICE DETAILS *******|");
+            System.out.println("Name = " + buffer);
+            System.out.println("Vendor ID = " + vendorId);
+            System.out.println("Product ID = " + productId);
+            System.out.println("SerialNumber = " + deviceDescriptor.iSerialNumber());
+            System.out.println("Manufacturer = " + deviceDescriptor.iManufacturer());
+
+            /* Setting the device detail */
+            Device.Builder builder = Device.newBuilder()
+                    .setDeviceName(buffer != null ? buffer.toString() : "")
+                    .setVendorId(vendorId)
+                    .setProductId(productId);
+
+            /* Add Device to list */
+            devices.add(builder.build());
+        }
         return devices;
     }
 }
